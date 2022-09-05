66a2967637b9706f5e7782ac26af53d0eeccb18d
Galo Sanchez <galoalexander.sr@gmail.com>
2022-09-05 17:09:07 -0500

    Desarrollo sección home

--- code-dev/scripts/script-index.js
+++ code-dev/scripts/script-index.js
@@ -5,7 +5,8 @@
 
 const scrollHeader = () => {
     const nav = document.getElementById('main-header');
-    this.scrollY >= 1 ? nav.classList.add('scroll-header') : nav.classList.remove('scroll-header')
+    this.scrollY >= 10 ? nav.classList.add('scroll-header-normal') : nav.classList.remove('scroll-header-normal');
+    this.scrollY >= 120 ? nav.classList.add('scroll-header-small') : nav.classList.remove('scroll-header-small');
 }
 
 window.addEventListener('scroll', scrollHeader)
